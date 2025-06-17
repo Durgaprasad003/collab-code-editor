5c043da35407ab5c26b788bf58818d982ae53ef3
durga prasad <kelladurgaprasad92@gmail.com>
2025-06-17 18:46:00 +0530

    U

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@
     const path = require('path');
     const { Server } = require('socket.io');
     const ACTIONS = require('./actions');
-
-
+    require('dotenv').config();
 
     const server = http.createServer(app);
     const io = new Server(server, {
@@ -67,7 +66,7 @@
         });
     });
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT
     server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.get("/", (req, res) => {
