a7c9a234c560612b9ba6420e196cb0a7508f9cf8
durga prasad <kelladurgaprasad92@gmail.com>
2025-06-17 16:34:47 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -1,88 +1,91 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const ACTIONS = require('./actions');
+    const express = require('express');
+    const app = express();
+    const http = require('http');
+    const path = require('path');
+    const { Server } = require('socket.io');
+    const ACTIONS = require('./actions');
 
 
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*", // frontend URL
-    methods: ["GET", "POST"],
-  },
-});
+    const server = http.createServer(app);
+    const io = new Server(server, {
+    cors: {
+        origin: "*", // frontend URL
+        methods: ["GET", "POST"],
+    },
+    });
 
 
-const userSocketMap = {};
-function getAllConnectedClients(roomid) {
-    // Map
-    return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
-}
+    const userSocketMap = {};
+    function getAllConnectedClients(roomid) {
+        // Map
+        return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map(
+            (socketId) => {
+                return {
+                    socketId,
+                    username: userSocketMap[socketId],
+                };
+            }
+        );
+    }
 
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+    io.on('connection', (socket) => {
+        console.log('socket connected', socket.id);
 
-    socket.on(ACTIONS.JOIN, ({ roomid, username }) => {
-        userSocketMap[socket.id] = username;
-        socket.join(roomid);
-        const clients = getAllConnectedClients(roomid);
-        console.log(clients)
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
+        socket.on(ACTIONS.JOIN, ({ roomid, username }) => {
+            userSocketMap[socket.id] = username;
+            socket.join(roomid);
+            const clients = getAllConnectedClients(roomid);
+            console.log(clients)
+            clients.forEach(({ socketId }) => {
+                io.to(socketId).emit(ACTIONS.JOINED, {
+                    clients,
+                    username,
+                    socketId: socket.id,
+                });
             });
+        });
+
+        socket.on(ACTIONS.CODE_CHANGE, ({ roomid, code }) => {
+            socket.in(roomid).emit(ACTIONS.CODE_CHANGE, { code });
+        });
+
+        socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+            io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+        });
+
+        socket.on('disconnecting', () => {
+            const rooms = [...socket.rooms];
+            rooms.forEach((roomid) => {
+                socket.in(roomid).emit(ACTIONS.DISCONNECTED, {
+                    socketId: socket.id,
+                    username: userSocketMap[socket.id],
+                });
+            });
+            delete userSocketMap[socket.id];
+            socket.leave();
         });
     });
 
-    socket.on(ACTIONS.CODE_CHANGE, ({ roomid, code }) => {
-        socket.in(roomid).emit(ACTIONS.CODE_CHANGE, { code });
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+    app.use(express.static(path.join(__dirname, "../realtime-editor/build")));
+
+
+    
+    app.get('/room', (req, res) => {
+    res.send('Backend is working!');
     });
 
-    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+
+
+
+
+
+
+
+
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../realtime-editor/build/index.html"));
     });
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomid) => {
-            socket.in(roomid).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
-        });
-        delete userSocketMap[socket.id];
-        socket.leave();
-    });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-app.get('/room', (req, res) => {
-  res.send('Backend is working!');
-});
-
-
-
-
-
-
-
-app.use(express.static(path.join(__dirname, "../realtime-editor/build")));
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../realtime-editor/build/index.html"));
-});
