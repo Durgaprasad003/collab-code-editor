c7a9bd37d39a0a6a675833844a8f45a5a687a02f
durga prasad <kelladurgaprasad92@gmail.com>
2025-06-17 17:36:14 +0530

    u

--- backend/server.js
+++ backend/server.js
@@ -70,3 +70,6 @@
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
+app.get("/", (req, res) => {
+  res.send("✅ Server is working!");
+});
