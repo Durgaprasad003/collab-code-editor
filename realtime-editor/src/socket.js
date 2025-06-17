0a1f2674d9981299c2d2609fbc20b1c0119e43d1
durga prasad <kelladurgaprasad92@gmail.com>
2025-06-17 18:46:26 +0530

    U

--- realtime-editor/src/socket.js
+++ realtime-editor/src/socket.js
@@ -4,7 +4,7 @@ let socket ;
 
 export const initsocket = async () => {
   socket = io('https://collab-code-editor-m4lr.onrender.com/', {
-    'force new connection':true,
+    
     transports: ["websocket"], // use websocket only
     reconnectionAttempts: 5,   // try to reconnect 5 times
     timeout: 10000,            // wait 10 seconds before failing
