562c08fd06b1e795db051d7cd05a95a257c2c6cd
durga prasad <kelladurgaprasad92@gmail.com>
2025-06-17 16:23:32 +0530

    added route

--- backend/server.js
+++ backend/server.js
@@ -67,6 +67,23 @@ io.on('connection', (socket) => {
     });
 });
 
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
+
+
+
+const path = require("path");
+
+
+
+app.use(express.static(path.join(__dirname, "../realtime-editor/build")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../realtime-editor/build/index.html"));
+});
